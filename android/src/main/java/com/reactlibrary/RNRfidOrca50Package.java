813dc1cf6e44c6ca8c65692ed10bc4ca190f9a39
Eric <eric@Erics-MacBook-Pro.local>
2019-03-23 21:58:02 +1100

    update

--- android/src/main/java/com/reactlibrary/RNRfidOrca50Package.java
+++ android/src/main/java/com/reactlibrary/RNRfidOrca50Package.java
@@ -10,19 +10,23 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.bridge.JavaScriptModule;
+
 public class RNRfidOrca50Package implements ReactPackage {
-    @Override
-    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(new RNRfidOrca50Module(reactContext));
-    }
+	@Override
+	public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+		return Arrays.<NativeModule>asList(new RNRfidOrca50Module(reactContext));
+	}
 
-    // Deprecated from RN 0.47
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-      return Collections.emptyList();
-    }
+	// Deprecated from RN 0.47
+	public List<Class<? extends JavaScriptModule>> createJSModules() {
+		return Collections.emptyList();
+	}
 
-    @Override
-    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-      return Collections.emptyList();
-    }
+	@Override
+	public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+		return Collections.emptyList();
+	}
+
+	private void Hi() {
+	}
 }
